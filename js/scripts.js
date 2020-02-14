6d58ba4de7d916fa07d93d09d25a69d037f5c264
felix <felixxnathann@gmail.com>
2020-02-14 17:58:57 +0300

    image

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,8 @@
-const maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 function validateInput() {
     let gender=document.forms["akanForm"]["gender"].value;
-    let year=document.forms["akanForm"]["year"].value;
-
-    if (year)=null && year != undefined && year !="") {
-        $("#akanModal").modal("show");
-        document.getElementById("actualAkanName").innerHTML=${getAkanName(
-            year,
-            gender
-        )};
-    }  {
-        alert("Please enter a valid birth date");
-    }
-}
-    var getAkanName=(year,gender)=> {
-        let day=new Date(year);
-        let dayNumber=day.getDay();
-        let name="";
-
-        if (dayNumber)=null) {
-            if (gender==="male"){
-                name=maleNames[dayNumber];
-            }else if (gender==="female"){
-                name=femaleNames[dayNumber};
-            }
-        }
-        return name;
-    };
+    let year=document.forms["akanForm"]["year"].value;}
 
 
-
-}
