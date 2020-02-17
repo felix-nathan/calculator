2896868475785020c34965a03a84ca23ee45b685
felix <felixxnathann@gmail.com>
2020-02-17 07:57:15 +0300

    delete css link in body

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
+var Days={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+alert("calculating")
 
 function validateInput() {
     let gender=document.forms["akanForm"]["gender"].value;
     let year=document.forms["akanForm"]["year"].value;}
+
+var getAkanName=(year,gender) 
+    let day=new Date(year);
+    let dayNumber=day.getDay();
+    let name="";
+    
+    alert("calculating")
+
+if (dayNumber !=null) [
+    if (gender==="male") {
+        name=
+    }
+]    
+
+
 
 
