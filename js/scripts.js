75436570d65684039fde8e09f62a705c385bfae3
felix <felixxnathann@gmail.com>
2020-02-17 08:17:50 +0300

    add info in form

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var Days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-function validateInput() {
+function getName() {
     let gender=document.forms["akanForm"]["gender"].value;
     let year=document.forms["akanForm"]["year"].value;}
 
