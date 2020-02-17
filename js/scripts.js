739733583d53cf2073cc071beb48ecca1a3c6c23
felix <felixxnathann@gmail.com>
2020-02-17 10:09:25 +0300

    add overlay class

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,33 @@
-var Days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var daysOfWeek=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
 
 function getName() {
-    let gender=document.forms["akanForm"]["gender"].value;
-    let year=document.forms["akanForm"]["year"].value;}
+    year = parseInt( document.getElementById("year").value);
+    month = parseInt( document.getElementById("month").value);
+    day = parseInt( document.getElementById("day").value);
+    male=document.getElementById("male");
+    female=document.getElementById("female");
 
-var getAkanName=(year,gender) 
-    let day=new Date(year);
-    let dayNumber=day.getDay();
-    let name="";
-    
-    alert("calculating")
+if(day<=0||day>31)
+alert("enter a valid day");
+else if(month<0||month>12 ||month ==2&& day>29)
+alert("enter a valid month");
+else if(year<0||year>2020)
+alert("enter a valid year");
 
-if (dayNumber !=null) [
-    if (gender==="male") {
-        name=
-    }
-]    
+var day = new Date(year + "/" + month + "/" + day);
 
+var birthday=day.getDay();
+
+if(male.checked==true){
+    alert("You were born on " + daysOfWeek[birthday]+ " and your akan name is " + maleNames[birthday])
+}
+if(female.checked==true){
+    alert("You were born on " + daysOfWeek[birthday]+ " and your akan name is " + femaleNames[birthday])
+}
+ 
+}
 
 
 
